649ed677753042683f9c9dece5b05b9d3c9cb2be
SRNT-GG <95472530+SRNT-GG@users.noreply.github.com>
2023-05-27 17:54:20 +0200

    Reworked expbartext.lua to be similar to repbartext.lua.

--- modules/expbartext.lua
+++ modules/expbartext.lua
@@ -7,8 +7,11 @@ else
     return 
 end
 
-if UnitLevel("player") < 80 then -- Nonsense to run any code on a bar that doesn't exist.
-    -- Expbar text variablesw
+function srntTools:updateExpBarText()
+    if UnitLevel("player") >= 80 then
+        return -- Expbar only visible when you're lower level than cap.
+    end
+
     local currentExp = UnitXP("player")
     local maxExp = UnitXPMax("player")
     local expPercent = currentExp / maxExp * 100
@@ -21,22 +24,18 @@ if UnitLevel("player") < 80 then -- Nonsense to run any code on a bar that doesn
     expText:SetJustifyH("CENTER")
     expText:SetJustifyV("MIDDLE")
     expText:SetText(string.format("Exp: %d / %d (%.2f%%)", currentExp, maxExp, expPercent))
-
-    local frame = CreateFrame("Frame")
-    frame:RegisterEvent("PLAYER_XP_UPDATE")
-    frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-    frame:SetScript("OnEvent", function(self, event, addonName)
-        -- Update exptext whenever exp is gained
-        currentExp = UnitXP("player")
-        maxExp = UnitXPMax("player")
-        expPercent = currentExp / maxExp * 100
-        remainingExp = maxExp - currentExp
-        expText:SetText(string.format("Exp: %d / %d (%.2f%%)", currentExp, maxExp, expPercent))
-    end)
-
-    MainMenuExpBar:HookScript("OnEnter", function(self)
-        -- Disable the default text & tooltip.
-        MainMenuBarExpText:Hide()
-        GameTooltip:Hide()
-    end)
 end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_XP_UPDATE")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function(self, event, addonName)
+     -- Update exptext whenever exp is gained
+    srntTools:updateExpBarText()
+end)
+
+MainMenuExpBar:HookScript("OnEnter", function(self)
+    -- Disable the default text & tooltip.
+    MainMenuBarExpText:Hide()
+    GameTooltip:Hide()
+end)
